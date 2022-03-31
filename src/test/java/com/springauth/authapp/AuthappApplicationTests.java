7af823e1fd3d6ec672a0f265d00ce24765a8e195
SergeitZ <sergiofranco6@gmail.com>
2022-03-31 19:21:48 -0400

    Initial commit

---
+++ src/test/java/com/springauth/authapp/AuthappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springauth.authapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
